42d92f13a6572b26d176b25bf699116709603873
MINJU-KIMmm <0112km@ewhain.net>
2022-06-18 23:28:00 +0900

    Feat : Add project

---
+++ src/main/java/com/steady/steadyback/SteadyBackApplication.java
@@ -0,0 +1,13 @@
+package com.steady.steadyback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteadyBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteadyBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/steady/steadyback/SteadyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steady.steadyback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteadyBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
