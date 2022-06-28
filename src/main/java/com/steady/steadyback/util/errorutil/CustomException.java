1afc968c2b80325d57d1066fb373de4051046ea8
MINJU-KIMmm <0112km@ewhain.net>
2022-06-28 15:14:59 +0900

    Feat : Error Handler 생성

---
+++ src/main/java/com/steady/steadyback/util/errorutil/CustomException.java
@@ -0,0 +1,10 @@
+package com.steady.steadyback.util.errorutil;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+    private final ErrorCode errorCode;
+}
