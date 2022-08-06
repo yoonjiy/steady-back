1d81b0dcaa41f90d2380725d1ca080fff65fbae5
MINJU-KIMmm <0112km@ewhain.net>
2022-08-06 16:27:19 +0900

    Fix : TimeZone 빈으로 주입

--- src/main/java/com/steady/steadyback/SteadyBackApplication.java
+++ src/main/java/com/steady/steadyback/SteadyBackApplication.java
@@ -4,9 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 public class SteadyBackApplication {
+
+    @PostConstruct
+    public void started() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SteadyBackApplication.class, args);
