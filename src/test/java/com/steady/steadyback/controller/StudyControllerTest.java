654bf9a6aa067a87cc7542270fbfa51cc5068978
MINJU-KIMmm <0112km@ewhain.net>
2022-09-25 17:38:18 +0900

    Test : 스터디 조회 테스트 코드 추가
    #53

--- src/test/java/com/steady/steadyback/controller/StudyControllerTest.java
+++ src/test/java/com/steady/steadyback/controller/StudyControllerTest.java
@@ -1,7 +1,51 @@
 package com.steady.steadyback.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class StudyControllerTest {
 
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void 스터디id로조회하기() throws Exception {
+
+        mvc.perform(get("/studies/53"))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+    }
+
+    @Test
+    public void 로그인안하고조회() throws Exception {
+        mvc.perform(get("/studies/53"))
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
+    }
 }
