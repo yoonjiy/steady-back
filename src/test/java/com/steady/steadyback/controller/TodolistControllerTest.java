bf07af7f437bbd4932b33d31c1a8e88c339e23a4
변지은 <bje5774@gmail.com>
2022-10-09 19:32:15 +0900

    Test : 투두리스트 테스트

--- src/test/java/com/steady/steadyback/controller/TodolistControllerTest.java
+++ src/test/java/com/steady/steadyback/controller/TodolistControllerTest.java
@@ -1,7 +1,31 @@
 package com.steady.steadyback.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class TodolistControllerTest {
 
+    @Autowired
+    private MockMvc mvc;
+    @Test
+    @WithUserDetails(value = "test5@efub.com", userDetailsServiceBeanName = "userService")
+    public void getTodoList() throws Exception{
+        mvc.perform(get("/todos"))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+    }
 }
