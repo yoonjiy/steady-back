ff68b31b8cd67d546a2ad1cdfb3cd3d19bddd286
MINJU-KIMmm <0112km@ewhain.net>
2022-10-31 22:15:18 +0900

    Feat : 로그아웃 관련 에러 코드 추가
    - 로그아웃 되어 Refresh Token이 존재하지 않는 경우에 대한 에러코드 추가
    - Chore : Bad Reqeust 관련 코드 위치 조정

--- src/main/java/com/steady/steadyback/util/errorutil/ErrorCode.java
+++ src/main/java/com/steady/steadyback/util/errorutil/ErrorCode.java
@@ -19,6 +19,10 @@ public enum ErrorCode {
     CANNOT_DUPLICATE_EMAIL(BAD_REQUEST, "이미 가입된 이메일입니다"),
     INCORRECT_PASSWORD(BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
     OVER_9_MEMBER(BAD_REQUEST, "스터디원은 최대 9명까지 추가 가능합니다."),
+    REFRESH_TOKEN_DOESNT_EXIST(BAD_REQUEST, "RefreshToken이 존재하지 않습니다."),
+    INVALID_TOKEN(BAD_REQUEST, "토큰이 만료됐거나 유효하지 않습니다."),
+    INVALID_REFRESH_TOKEN(BAD_REQUEST, "리프레시 토큰이 유효하지 않습니다. 재로그인 해주세요."),
+    REFRESH_TOKEN_DOESNT_MATCH(BAD_REQUEST, "리프레시 토큰이 매치되지 않습니다."),
 
     //404 NOT_FOUND : Resource를 찾을 수 없음
     MEMBER_NOT_FOUND(NOT_FOUND, "해당 유저 정보를 찾을 수 없습니다."),
@@ -36,9 +40,6 @@ public enum ErrorCode {
 
     USER_STUDY_ALREADY_EXISTS(CONFLICT, "이미 가입한 스터디원입니다."),
 
-    INVALID_TOKEN(BAD_REQUEST, "토큰이 만료됐거나 유효하지 않습니다."),
-    INVALID_REFRESH_TOKEN(BAD_REQUEST, "리프레시 토큰이 유효하지 않습니다. 재로그인 해주세요."),
-    REFRESH_TOKEN_DOESNT_MATCH(BAD_REQUEST, "리프레시 토큰이 매치되지 않습니다.")
     ;
 
     private final HttpStatus httpStatus;
